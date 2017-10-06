f42409f88688ac0efd96a4882d931fac9ef9664a
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:52:02 +0100

    Added unit test for rook movement

--- tests/engine/pieces/rook.spec.js
+++ tests/engine/pieces/rook.spec.js
@@ -1,5 +1,36 @@
+import 'chai/register-should';
 import Rook from '../../../src/engine/pieces/rook';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('Rook', () => {
 
+    let board;
+    beforeEach(() => board = new Board());
+
+    it('can move laterally', () => {
+        const rook = new Rook(Player.WHITE);
+        board.setPiece(Square.at(1, 2), rook);
+
+        const moves = rook.getAvailableMoves(board);
+
+        const expectedMoves = [
+            // Horizontal
+            Square.at(1, 0), Square.at(1, 1), Square.at(1, 3), Square.at(1, 4), Square.at(1, 5), Square.at(1, 6), Square.at(1, 7),
+            // Vertical
+            Square.at(0, 2), Square.at(2, 2), Square.at(3, 2), Square.at(4, 2), Square.at(5, 2), Square.at(6, 2), Square.at(7, 2)
+        ];
+
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('cannot make any other moves', () => {
+        const rook = new Rook(Player.WHITE);
+        board.setPiece(Square.at(1, 2), rook);
+
+        const moves = rook.getAvailableMoves(board);
+
+        moves.should.have.length(14);
+    });
 });
