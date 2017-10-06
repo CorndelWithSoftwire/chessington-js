1dbe586ed788c7dfe9d1ceaee242122dd4da2dd2
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:11:19 +0100

    Added tests for queen movement

--- tests/engine/pieces/queen.spec.js
+++ tests/engine/pieces/queen.spec.js
@@ -1,5 +1,53 @@
+import 'chai/register-should';
 import Queen from '../../../src/engine/pieces/queen';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
+import GameSettings from '../../../src/engine/gameSettings';
 
 describe('Queen', () => {
 
+    let board;
+    beforeEach(() => board = new Board());
+
+    it('can move laterally', () => {
+        const queen = new Queen(Player.WHITE);
+        board.setPiece(Square.at(2, 3), queen);
+
+        const moves = queen.getAvailableMoves(board);
+
+        const expectedMoves = [
+            // Horizontal
+            Square.at(2, 0), Square.at(2, 1), Square.at(2, 2), Square.at(2, 4), Square.at(2, 5), Square.at(2, 6), Square.at(2, 7),
+            // Vertical
+            Square.at(0, 3), Square.at(1, 3), Square.at(3, 3), Square.at(4, 3), Square.at(5, 3), Square.at(6, 3), Square.at(7, 3)
+        ];
+        
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('can move diagonally', () => {
+        const queen = new Queen(Player.WHITE);
+        board.setPiece(Square.at(2, 3), queen);
+
+        const moves = queen.getAvailableMoves(board);
+
+        const expectedMoves = [
+            // Forwards diagonal
+            Square.at(0, 1), Square.at(1, 2), Square.at(3, 4), Square.at(4, 5), Square.at(5, 6), Square.at(6, 7),
+            // Backwards diagonal
+            Square.at(0, 5), Square.at(1, 4), Square.at(3, 2), Square.at(4, 1), Square.at(5, 0)
+        ];
+        
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('cannot make any other moves', () => {
+        const queen = new Queen(Player.WHITE);
+        board.setPiece(Square.at(2, 3), queen);
+
+        const moves = queen.getAvailableMoves(board);
+
+        moves.should.have.length(25);
+    });
 });
