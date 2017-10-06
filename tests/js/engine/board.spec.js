4594333b976ee5527f6bbb56cb5555fef94b1f0d
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 11:00:03 +0100

    Add example tests for the Board class

---
+++ tests/js/engine/board.spec.js
@@ -0,0 +1,41 @@
+import 'chai/register-should';
+import Board from '../../../src/js/engine/board';
+import Pawn from '../../../src/js/engine/pieces/pawn';
+import Player from '../../../src/js/engine/player';
+import Square from '../../../src/js/engine/square';
+
+describe('Board', () => {
+
+    describe('pawns', () => {
+
+        let board;
+        beforeEach(() => { // Common code executed before each test.
+            board = new Board();
+        });
+
+        it('can be added to the board', () => {
+            // Arrange
+            const pawn = new Pawn(Player.WHITE);
+            const square = Square.at(0, 0);
+
+            // Act
+            board.setPiece(square, pawn);
+
+            // Assert
+            board.getPiece(square).should.equal(pawn); // Object equality: same object reference
+        });
+
+        it('can be found on the board', () => {
+            // Arrange
+            const pawn = new Pawn(Player.WHITE);
+            const square = Square.at(6, 4);
+
+            // Act
+            board.setPiece(square, pawn);
+
+            // Assert
+            board.findPiece(pawn).should.eql(square); // Object equivalence: different objects, same data
+        });
+
+    });
+});
