38202d94a4a02f1dc7631054e1d509551a01455a
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:27:29 +0100

    Added a test for king movement

--- tests/engine/pieces/king.spec.js
+++ tests/engine/pieces/king.spec.js
@@ -1,5 +1,34 @@
+import 'chai/register-should';
 import King from '../../../src/engine/pieces/king';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('King', () => {
 
+    let board;
+    beforeEach(() => board = new Board());
+
+    it('can move to adjacent squares', () => {
+        const king = new King(Player.WHITE);
+        board.setPiece(Square.at(3, 4), king);
+
+        const moves = king.getAvailableMoves(board);
+
+        const expectedMoves = [
+            Square.at(2, 3), Square.at(2, 4), Square.at(2, 5), Square.at(3, 5),
+            Square.at(4, 5), Square.at(4, 4), Square.at(4, 3), Square.at(3, 3)
+        ];
+
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('cannot make any other moves', () => {
+        const king = new King(Player.WHITE);
+        board.setPiece(Square.at(3, 4), king);
+
+        const moves = king.getAvailableMoves(board);
+
+        moves.should.have.length(8);
+    });
 });
