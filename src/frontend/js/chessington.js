20e4cab8411ed4b78a086ddc25a220583c4b94fb
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 10:17:44 +0100

    Add a simple chess UI to the starter project

---
+++ src/frontend/js/chessington.js
@@ -0,0 +1,10 @@
+function createChessBoard() {
+    var chessBoard = ChessBoard(
+        'chess-board', 
+        { 
+            position: 'start', 
+            showNotation: false,
+            draggable: true
+        }
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello, Chessington!');
+import express from 'express';
+import { join } from 'path';
+const app = express();
+
+app.use(express.static(join(__dirname, 'frontend')));
+
+app.listen(3000, () => console.log('Chessington is ready to go!'));
