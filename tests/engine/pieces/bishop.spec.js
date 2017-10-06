bce1eb8d8ecd7609494d8bc13f031324583b927d
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:55:29 +0100

    Add a test that bishops can take pieces

--- tests/engine/pieces/bishop.spec.js
+++ tests/engine/pieces/bishop.spec.js
@@ -1,6 +1,7 @@
 import 'chai/register-should';
 import Bishop from '../../../src/engine/pieces/bishop';
 import Pawn from '../../../src/engine/pieces/pawn';
+import King from '../../../src/engine/pieces/king';
 import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
 import Board from '../../../src/engine/board';
@@ -55,5 +56,38 @@ describe('Bishop', () => {
         const moves = bishop.getAvailableMoves(board);
 
         moves.should.not.deep.include(Square.at(7, 7));
+    });
+
+    it('can take opposing pieces', () => {
+        const bishop = new Bishop(Player.WHITE);
+        const opposingPiece = new Pawn(Player.BLACK);
+        board.setPiece(Square.at(4, 4), bishop);
+        board.setPiece(Square.at(6, 6), opposingPiece);
+
+        const moves = bishop.getAvailableMoves(board);
+
+        moves.should.deep.include(Square.at(6, 6));
+    });
+
+    it('cannot take the opposing king', () => {
+        const bishop = new Bishop(Player.WHITE);
+        const opposingKing = new King(Player.BLACK);
+        board.setPiece(Square.at(4, 4), bishop);
+        board.setPiece(Square.at(6, 6), opposingKing);
+
+        const moves = bishop.getAvailableMoves(board);
+
+        moves.should.not.deep.include(Square.at(6, 6));
+    });
+
+    it('cannot take friendly pieces', () => {
+        const bishop = new Bishop(Player.WHITE);
+        const friendlyPiece = new Pawn(Player.WHITE);
+        board.setPiece(Square.at(4, 4), bishop);
+        board.setPiece(Square.at(6, 6), friendlyPiece);
+
+        const moves = bishop.getAvailableMoves(board);
+
+        moves.should.not.deep.include(Square.at(6, 6));
     });
 });
