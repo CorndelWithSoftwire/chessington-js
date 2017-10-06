44b31c3a1e5c12ca96731bea6b162a60db40d3d7
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:22:44 +0100

    Added a test for knight movement

--- tests/engine/pieces/knight.spec.js
+++ tests/engine/pieces/knight.spec.js
@@ -1,5 +1,48 @@
+import 'chai/register-should';
 import Knight from '../../../src/engine/pieces/knight';
+import Pawn from '../../../src/engine/pieces/pawn';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('Knight', () => {
 
+    let board;
+    beforeEach(() => board = new Board());
+
+    it('can make knights moves', () => {
+        const knight = new Knight(Player.WHITE);
+        board.setPiece(Square.at(4, 4), knight);
+
+        const moves = knight.getAvailableMoves(board);
+
+        const expectedMoves = [
+            Square.at(2, 5), Square.at(2, 3), Square.at(3, 6), Square.at(3, 2),
+            Square.at(5, 6), Square.at(5, 2), Square.at(6, 5), Square.at(6, 3)
+        ];
+
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('cannot make any other moves', () => {
+        const knight = new Knight(Player.WHITE);
+        board.setPiece(Square.at(4, 4), knight);
+
+        const moves = knight.getAvailableMoves(board);
+
+        moves.should.have.length(8);
+    });
+
+    it('can jump over other pieces', () => {
+        const knight = new Knight(Player.WHITE);
+        const firstPawn = new Pawn(Player.WHITE);
+        const secondPawn = new Pawn(Player.BLACK);
+        board.setPiece(Square.at(4, 4), knight);
+        board.setPiece(Square.at(3, 4), firstPawn);
+        board.setPiece(Square.at(3, 5), secondPawn);
+
+        const moves = knight.getAvailableMoves(board);
+
+        moves.should.deep.include(Square.at(2, 5));
+    });
 });
