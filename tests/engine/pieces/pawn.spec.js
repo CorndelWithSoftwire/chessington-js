18d43a1bc70bcc320834dbf1fb87c44f27f3e18d
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 12:39:53 +0100

    Added a test for pawns moving one square

--- tests/engine/pieces/pawn.spec.js
+++ tests/engine/pieces/pawn.spec.js
@@ -1,5 +1,38 @@
+import 'chai/register-should';
 import Pawn from '../../../src/engine/pieces/pawn';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('Pawn', () => {
+
+    let board;
+    beforeEach(() => board = new Board());
+
+    describe('white pawns', () => {
+        
+        it('can move one square up', () => {
+            const pawn = new Pawn(Player.WHITE);
+            board.setPiece(Square.at(0, 0), pawn);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.deep.include(Square.at(1, 0));
+        });
+
+    });
+
+    describe('black pawns', () => {
+        
+        it('can move one square down', () => {
+            const pawn = new Pawn(Player.BLACK);
+            board.setPiece(Square.at(7, 7), pawn);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.deep.include(Square.at(6, 7));
+        });
+
+    });
 
 });
