8025aa7be2eb058875dc6c1525f686a7146d2209
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-10 14:43:56 +0100

    Made rooks able to move laterally

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,16 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const location = board.findPiece(this);
+
+        const horizontalMoves = Array.from(Array(GameSettings.BOARD_SIZE).keys())
+            .filter(ix => ix !== location.col)
+            .map(ix => Square.at(location.row, ix));
+
+        const verticalMoves = Array.from(Array(GameSettings.BOARD_SIZE).keys())
+            .filter(ix => ix !== location.row)
+            .map(ix => Square.at(ix, location.col));
+
+        return horizontalMoves.concat(verticalMoves);
     }
 }
