d61e8d4d9c6c2cec9118c2220697aa3d1a21ea7c
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-10 15:12:13 +0100

    Made knights able to move

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,14 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const location = board.findPiece(this);
+        const row = location.row, col = location.col;
+
+        return [
+            Square.at(row + 1, col + 2), Square.at(row + 1, col - 2),
+            Square.at(row - 1, col + 2), Square.at(row - 1, col - 2),
+            Square.at(row + 2, col + 1), Square.at(row + 2, col - 1),
+            Square.at(row - 2, col + 1), Square.at(row - 2, col - 1)
+        ];
     }
 }
