752f5b8f4e99dcf20489925d9d19faea4acc3712
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-10 15:14:54 +0100

    Made kings able to move

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +7,14 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const location = board.findPiece(this);
+        const row = location.row, col = location.col;
+
+        return [
+            Square.at(row + 1, col + 1), Square.at(row + 1, col),
+            Square.at(row + 1, col - 1), Square.at(row, col - 1),
+            Square.at(row - 1, col - 1), Square.at(row - 1, col),
+            Square.at(row - 1, col + 1), Square.at(row, col + 1)
+        ];
     }
 }
