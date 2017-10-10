975e58c4a7da238b7f7647711c7d47ee1d60be5b
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-10 15:43:30 +0100

    Made pawns able to capture pieces

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -14,12 +14,20 @@ export default class Pawn extends Piece {
         const singleMoveSquare = Square.at(location.row + delta, location.col);
         const doubleMoveSquare = Square.at(location.row + 2 * delta, location.col);
 
+        let normalMoves;
         if (!board.squareInBounds(singleMoveSquare) || board.squareIsOccupied(singleMoveSquare)) {
-            return [];
+            normalMoves = [];
         } else if (!board.squareInBounds(doubleMoveSquare) || board.squareIsOccupied(doubleMoveSquare) || this.hasMoved) {
-            return [singleMoveSquare];
+            normalMoves = [singleMoveSquare];
         } else {
-            return [singleMoveSquare, doubleMoveSquare];
+            normalMoves = [singleMoveSquare, doubleMoveSquare];
         }
+
+        const captureMoves = [
+            Square.at(location.row + delta, location.col + 1), 
+            Square.at(location.row + delta, location.col - 1)
+        ].filter(square => board.squareInBounds(square) && board.squareIsOccupied(square) && this._isCapturable(board.getPiece(square)));
+
+        return normalMoves.concat(captureMoves);
     }
 }
